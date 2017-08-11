322fadd5fdc3866f88bd5c6e057e7e454978f3ad
Stefan Tien <stefan55831@gmail.com>
2017-08-11 16:34:27 +0800

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,7 @@ const { ObjectID } = require("mongodb");
 
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
+const { User } = require("./../models/user");
 const { todos, populateTodos, users, populateUsers } = require("./seed/seed");
 
 beforeEach(populateUsers);
@@ -224,5 +225,56 @@ describe("POST /users", () => {
       .send({ email, password })
       .expect(400)
       .end(done);
+  });
+});
+
+describe("POST /users/login", () => {
+  it("should login and return auth token", (done) => {
+    request(app)
+      .post("/users/login")
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.header["x-auth"]).toExist();
+      }).
+      end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: "auth",
+            token: res.header["x-auth"]
+          });
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it("should reject invalid login", (done) => {
+    request(app)
+      .post("/users/login")
+      .send({
+        email: "wrongemail",
+        password: "1234567890"
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.header["x-auth"]).toNotExist();
+      }).
+      end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((err) => done(err));
+      });
   });
 });
